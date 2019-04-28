ba7604af4a5eb9269f8b8bae79eb8ca26a595dcd
Jephthah <inagbotamuno@gmail.com>
2019-04-28 22:15:53 +0100

    Update Specialist.java

--- Specialist.java
+++ Specialist.java
@@ -9,36 +9,22 @@
  * 
  * */
 
-
 public class Specialist extends Doctor {
 
-    private int yearsofExperience;
     
     private String specialty;
 
     
     
-    public Specialist(String dname, String did, String dlevel, int slexp, String slspec, String i) {
+    public Specialist(String dname, String did, String dlevel, int slexp, String slspec) {
         
-    	super(dname, did, dlevel);
-        
-    	this.yearsofExperience = slexp;
+    	super(dname, did, dlevel, slexp);
+
         
     	this.specialty = slspec;
     
     }
 
-    public int getYearsofExperience() {
-        
-    	return yearsofExperience;
-    	
-    }
-
-    public void setYearsofExperience(int yearsofExperience) {
-        
-    	this.yearsofExperience = yearsofExperience;
-    	
-    }
 
     public String getSpecialty() {
         
